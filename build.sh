209137f38c591b46fe0f1cd9cc542d0431f9c481
Kraito <grigor4uk.pawel@yandex.ru>
2026-07-19 06:33:09 +0900

    ss

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-# Останавливать выполнение при любой ошибке (например, если код не скомпилировался)
+# Останавливать выполнение при любой ошибке
 set -e 
 
-# Функция для сборки, чтобы не писать одно и то же дважды
 build_and_hash() {
     local TAG=$1
     local OUTPUT_NAME="rfad-installer-$TAG"
@@ -15,16 +14,22 @@ build_and_hash() {
     # Высчитываем хэш
     local HASH=$(sha256sum "$OUTPUT_NAME" | awk '{print $1}')
 
-    # Переименовываем
-    local NEW_NAME="$OUTPUT_NAME-$HASH"
-    mv "$OUTPUT_NAME" "$NEW_NAME"
+    # Имя для файла-маркера
+    local HASH_FILENAME="sha256-${OUTPUT_NAME}-${HASH}"
 
-    echo "==> Успешно: $NEW_NAME"
+    # Создаем пустой файл с хэшем в названии
+    touch "$HASH_FILENAME"
+
+    echo "==> Успешно собран: $OUTPUT_NAME"
+    echo "==> Создан маркер: $HASH_FILENAME"
     echo "------------------------------------------------"
 }
 
+# Очистка старых билдов и старых файлов с хэшами
 rm -f rfad-installer-*
+rm -f sha256-*
 
+# Запускаем сборки
 build_and_hash "gdrive"
 build_and_hash "cdn"
 
